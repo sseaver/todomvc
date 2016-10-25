2267506c8a0f100beec694f57c43bfc3e983abd7
sseaver <sseaver321@gmail.com>
2016-10-25 14:08:21 -0400

    first api

--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from todo.models import Todo
+from todo.serializers import TodoSerializer
 # Create your views here.
+
+
+class TodoListAPIView(ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
--- todomvc/todomvc/urls.py
+++ todomvc/todomvc/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from todo.views import TodoListAPIView, TodoDetailAPIView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^api/todos/$', TodoListAPIView.as_view(), name="todo_list_api_view"),
+    url(r'^api/todos/(?P<pk>\d+)$', TodoDetailAPIView.as_view(), name="todo_detail_api_view"),
 ]
